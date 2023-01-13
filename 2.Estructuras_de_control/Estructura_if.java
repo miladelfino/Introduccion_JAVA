c4c884e24366805774b85695290ee4e717ac9d49
miladelfino <miladelfino17@gmail.com>
2023-01-13 17:56:29 -0300

    Create Estructura_if.java

---
+++ 2.Estructuras_de_control/Estructura_if.java
@@ -0,0 +1,20 @@
+public class EstructuraIf{
+    public static void main(String[] args) {
+     //Declaro una variable y la inicializo en la misma linea
+     int num = 1;
+
+     //Estructura if
+     if (num%2 == 0){
+     System.out.println("El número es par");}
+     else{ 
+     System.out.println("El número es impar");}
+
+     //Else habilita la rama del falso
+
+     System.out.println("*** El programa ha finalizado ***");
+
+
+
+    }
+    
+}
