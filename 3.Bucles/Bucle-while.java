75f058f9e4c4d6d539fb1a4f9cba24d2bfde3100
miladelfino <miladelfino17@gmail.com>
2023-01-14 19:27:53 -0300

    Create Bucle-while.java

---
+++ 3.Bucles/Bucle-while.java
@@ -0,0 +1,14 @@
+public class BucleWhile{
+    public static void main(String[] args) {
+
+    //Declaracion de variable e inicializacion
+    int num = 0;    
+
+    //Bucle while
+    while(num <= 10){
+        System.out.println(num);
+        num+=2;
+    }
+    System.out.println("*** El programa ha finalizado ***");
+    }
+}
