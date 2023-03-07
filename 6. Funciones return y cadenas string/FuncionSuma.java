267a1a1365784d7e35ccba5fd9b6bf38ed203fa5
miladelfino <miladelfino17@gmail.com>
2023-03-07 19:05:26 -0300

    Create FuncionSuma.java

---
+++ 6. Funciones return y cadenas string/FuncionSuma.java
@@ -0,0 +1,18 @@
+public class FuncionSuma {
+
+static double funcionSuma(int num1, double num2){
+    //1-Declaracion. inicializacion de VL
+    double resultado = num1 + num2;
+    //Retorno
+    return resultado;
+}
+
+
+public static void main(String[] args) {
+    //Llamamos a nuestra funcion
+    System.out.println(funcionSuma(10,5));
+
+}
+    
+
+}
