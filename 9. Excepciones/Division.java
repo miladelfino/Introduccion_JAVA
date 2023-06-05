411a550ff8991179eb0ac07533c164e58d84af20
miladelfino <miladelfino17@gmail.com>
2023-06-04 21:17:27 -0300

    Create Division.java

---
+++ 9. Excepciones/Division.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * @Documented Tema Excepciones
+ * @dominio Codo a Codo Inicial 2022
+ * @author Fernando Lucena
+ * */
+
+
+/*
+* Realizaremos una función división, la dividiremos por 0 y observaremos el resultado.
+* */
+
+public class Division {
+    //Zona de funciones
+    static double division (double dividendo, double divisor){
+        double aux;
+        aux = dividendo/divisor;
+        return aux;
+    }
+
+    public static void main(String[] args) {
+        //Objeto Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Variables
+        double dividendo, divisor, resultado;
+
+        //Salida-Entrada
+        System.out.println("Ingrese un numero");
+        dividendo = sc.nextDouble();
+        System.out.println("Ingrese otro numero");
+        divisor = sc.nextDouble();
+
+        //Proceso
+            resultado = division(dividendo, divisor);
+            System.out.println("El resultado es " + resultado);
+    }
+}
