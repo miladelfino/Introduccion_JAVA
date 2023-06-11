30badcaf314e2e38a620fa82f527bd7291f28af2
miladelfino <miladelfino17@gmail.com>
2023-06-11 10:48:11 -0300

    Create LeerObjetoLista.java

---
+++ 12. Biblioteca IO objetos/LeerObjetoLista.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+/*
+* Lectura de un archivo de un objeto ArrayList
+* El proceso de lectura es paralelo al proceso de escritura,
+* por lo que leer objetos del flujo de entrada  ObjectInputStream es muy simple en este práctico
+* veremos sus pasos.
+* */
+
+public class LeerObjetoLista {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //1- Creamos un fujo de entrada a disco, pasándole el nombre del archivo en disco al objeto de la clase File.
+        File archivo = new File("miLista.dat");
+        FileInputStream archivoEntrada = new FileInputStream(archivo);
+
+        //2- El fujo de entrada ObjectInputStream es el que procesa los datos
+        // y se vincula al objeto fileInputStrean que se llama archivoEntrada.
+        ObjectInputStream objetoEntrada = new ObjectInputStream(archivoEntrada);
+
+        //3- El método readObject lee los objetos del flujo de entrada,
+        ArrayList<String> listaCompras = (ArrayList<String>) objetoEntrada.readObject();
+
+        //4- Realizamos alguna tarea con el objeto por ejemplo agragamos un campo mas y lo 
+        System.out.println(listaCompras);
+        
+        //5-Se cierran los flujos
+        archivoEntrada.close();
+        objetoEntrada.close();
+    }
+}
