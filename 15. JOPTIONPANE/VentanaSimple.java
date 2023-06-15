6f165ebb9df1b5e375e5f275f3dcbab1eb484ae4
miladelfino <miladelfino17@gmail.com>
2023-06-15 19:43:23 -0300

    Create VentanaSimple.java

---
+++ 15. JOPTIONPANE/VentanaSimple.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class VentanaSimple {
+public static void main(String[] args) {
+
+JOptionPane.showMessageDialog(null, "Está a punto de hacer algo de lo que se puede arrepentir","Advertencia",1);
+JOptionPane.showMessageDialog(null, "Hola comision 22915","Saludo",-1);
+JOptionPane.showMessageDialog(null, "Ocurrió un error en la operación","Error",0);
+JOptionPane.showMessageDialog(null, "Presione OK para continuar","Mensaje interactivo",2);
+JOptionPane.showMessageDialog(null, "Esto es un mensaje de ayuda","Ayuda",3);
+
+}
+}
