2cc68fd4857ee20f34dccaeaaa3941fb4da97598
miladelfino <miladelfino17@gmail.com>
2023-06-15 19:29:45 -0300

    Create Ventana.java

---
+++ 14. Libreria Grafica/Ventana.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Ventana {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hola Comision Codo a Codo");
+    }
+}
